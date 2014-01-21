e688a73ac9a865d1bc997f20fb9f9497e0ce3df8
bierig <bierig@ifs.tuwien.ac.at>
2014-01-21 15:43:27 +0100

    Some modification toward a generalization of the code base. CLEF2011 collection still works from the plug-in folder.

--- MUCKE/src/mucke/query/QueryReader.java
+++ MUCKE/src/mucke/query/QueryReader.java
@@ -3,7 +3,11 @@ package mucke.query;
 /** Transforms a query string into a Query object */
 public interface QueryReader {
 
-    /** Transforms a query string into a Query object */
+    /** 
+     * Transforms a query string into a Query object 
+     * @param String query
+     * @return Query
+     */
     public abstract Query prepare(String query);
     
 }
