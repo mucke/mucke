874810e02846f5a4ba07e50a5896ae03894baa2a
bierig <bierig@ifs.tuwien.ac.at>
2014-02-13 17:28:40 +0100

    Moved ClassifierModel in concept package, somehow this has been overlooked in the refactoring face of the documentmodel package but had no impact since ClassifierModel is currently not used.

---
+++ MUCKE/src/mucke/concept/ClassifierModel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mucke.concept;
+
+/**
+ * Determines a visual concept based on a binary state (e.g. a hidden Markov model)
+ *
+ * @author Alexandra-Mihaela Siriteanu
+ */
+public class ClassifierModel {
+    
+}
