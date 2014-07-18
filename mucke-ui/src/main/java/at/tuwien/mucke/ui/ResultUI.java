16e0c39cbf11c2d3b592e02b2c2a8212efc2645e
bierig <bierig@ifs.tuwien.ac.at>
2014-07-18 15:30:20 +0200

    Minor update in the GUI that was done during ICMR and not yet commited.

---
+++ mucke-ui/src/main/java/at/tuwien/mucke/ui/ResultUI.java
@@ -0,0 +1,30 @@
+package at.tuwien.mucke.ui;
+
+/**
+ * Created by georgios on 3/20/14.
+ */
+public class ResultUI {
+    private String url;
+    private String title;
+
+    public ResultUI(String url, String title) {
+        this.url = url;
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
